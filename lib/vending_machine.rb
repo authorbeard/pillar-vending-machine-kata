a027db963a8a9e71620659d0874aa5e45895104a
authorbeard <msnolan@gmail.com>
2016-12-30 13:11:51 -0500

    successfully rejecting pennies

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -2,8 +2,15 @@ require 'byebug'
 
 class VendingMachine
 
+  COINS = {}
 
   def initialize
   end
 
+  def accept_coins(coin_name)
+    if coin_name.downcase == "penny"
+      return "I can't accept pennies, bub."
+    end
+
+  end
 end
